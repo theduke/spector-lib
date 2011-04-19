05567a1a5c8d87859f93c6c33379d88d14854b0a
theduke <christoph.herzog@theduke.at>
2011-04-19 14:35:33 +0200

    Fixed BaseWriter fromArray to use new Formatter classes.

--- lib/php53/Spector/Writer/BaseWriter.php
+++ lib/php53/Spector/Writer/BaseWriter.php
@@ -16,6 +16,20 @@ abstract class BaseWriter
 	
 	public function fromArray(array $arr)
 	{
+		if (isset($arr['formatter']))
+		{
+			$type = $arr['formatter'];
+			
+			// try to trigger autoloading for class
+			// php wont autoload when instantiating with variable as class name!
+			$className = ($type[0] === '\\') ? substr($type, 1) : $type;
+			class_exists($className, true);
+			
+			$this->setFormatter(new $type());
+			
+			unset($arr['formatter']);
+		}
+		
 		foreach ($arr as $property => $value)
 		{
 			$setter = "set" . ucfirst($property);
