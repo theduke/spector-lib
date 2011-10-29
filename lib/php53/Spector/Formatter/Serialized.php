499ab3f315fc9ddd8e84b5c5ceae27b589426a42
Christoph Herzog <chris@theduke.at>
2011-10-29 19:47:06 +0200

    Made Formatter\Serialized separator configurable

--- lib/php53/Spector/Formatter/Serialized.php
+++ lib/php53/Spector/Formatter/Serialized.php
@@ -6,9 +6,30 @@ use Spector\Writer\Writable;
 
 class Serialized implements Formatter
 {
+	const SEPARATOR = '-|-|-|-|-|-|-|-|-|-|-|-|-';
+	
+	protected $_separator = self::SEPARATOR;
+	
 	public function format(Writable $entry)
 	{
-		// escape newlines
-		return str_replace("\n", '\n', serialize($entry->toArray())) . PHP_EOL;
+		$output = serialize($entry->toArray());
+		
+		// if newline is separator, escape it
+		if ($this->_separator === "\n") 
+		{
+			$output = str_replace("\n", "\\n", $output);
+		}
+		
+		return $output . $this->_separator;
+	}
+	
+	public function setSeparator($sep)
+	{
+		$this->_separator = $sep;
+	}
+	
+	public function getSeparator()
+	{
+		return $this->_separator;
 	}
 }
